59f0e202c34085bb0af81954f5843cfd15dedff6
ngtuan092 <nguyenngoctuan092@gmail.com>
2022-01-13 21:19:25 +0700

    sửa lại biến

--- api/v1/controllers/filmController.js
+++ api/v1/controllers/filmController.js
@@ -3,19 +3,19 @@ const error_msg = require('../utils')
 const db = require('../db')
 module.exports = {
     getFilms: (req, res, next) => {
-        db.query('select count(maphim) from phim').then((num_film) => {
+        const theloai = req.query.theloai;
+        var condition = ""
+        if (theloai) {
+        condition = ` where json_search(theloai, 'all', "${theloai}") is not null`
+        }    
+        db.query('select count(maphim) from phim' + condition).then((num_film) => {
             const page = parseInt(req.query.page) || 0
-            const theloai = req.query.theloai;
             const filmPerPage = req.query.skip || 10
-            var condition = ""
-            if (theloai) {
-                condition = ` where json_search(theloai, 'all', "${theloai}") is not null`
-            }
             const maxLength = num_film[0]['count(maphim)']
-            if (maxLength < filmPerPage * (page - 1))
+            if (maxLength < filmPerPage * page)
                 return res.send('Invalid page')
             // bổ sung đánh giá, bia
-            db.query('select danhgia, bia, maphim ma, tenphim ten, thoigian, theloai, ngonngu, rate, trailer, date_format(khoi_chieu, "%d/%m/%Y") khoichieu, ghichu noidung from phim limit ?, ?' + condition, [filmPerPage * page, filmPerPage * (page + 1)])
+            db.query('select danhgia, bia, maphim ma, tenphim ten, thoigian, theloai, ngonngu, rate, trailer, date_format(khoi_chieu, "%d/%m/%Y") khoichieu, ghichu noidung from phim' + condition + ' limit ?, ?', [filmPerPage * page, filmPerPage])
                 .then((results) => {
                     return res.json({ results, maxLength })
                 })
@@ -28,7 +28,7 @@ module.exports = {
         var maphim = req.params.maphim
         db.query('select danhgia, bia, maphim ma, tenphim ten, thoigian, theloai, ngonngu, rate, trailer, date_format(khoi_chieu, "%d/%m/%Y") khoichieu, ghichu noidung from phim where maphim = ?', [maphim])
             .then((rows) => {
-                return res.json({ results: rows[0] })
+                return res.json({ result: rows[0] })
             })
             .catch((err) => {
                 console.log(err)
@@ -37,6 +37,8 @@ module.exports = {
     },
     getFutureFilms: async (req, res, next) => {
         const theloai = req.query.theloai;
+        const page = req.query.page || 0;
+        const skip = req.query.skip || 10
         var condition = ""
         if (theloai) {
             condition = ` and json_search(theloai, 'all', "${theloai}") is not null`
@@ -45,7 +47,7 @@ module.exports = {
 
             const results = await db.query('select danhgia, bia, maphim ma, tenphim ten, thoigian, theloai, ngonngu, rate, trailer, date_format(khoi_chieu, "%d/%m/%Y") khoichieu, ghichu noidung from phim where convert_tz(khoi_chieu, "+07:00", "+00:00") > date(now())' + condition)
             const maxLength = results.length
-            return res.json({ results, maxLength })
+            return res.json({ results: results.slice(page * skip, (page + 1) * skip), maxLength })
         }
         catch (e) {
             return res.json({err: e})
